c21ea4e7fc9d3f2cb952a1b0047986794e6f9626
ejaworska <ewel.jaworska.com>
2019-09-18 13:41:26 +0200

    Updates

---
+++ precache-manifest.6a99714caccc686bcc5625b0ca1cc4e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7074851774e98ce8fdf84e93683850",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "7f2a307c3d0f57453cd8",
+    "url": "/photo-gallery/static/css/main.47e98a6b.chunk.css"
+  },
+  {
+    "revision": "1acb5461c59a6d972919",
+    "url": "/photo-gallery/static/js/2.33eaed86.chunk.js"
+  },
+  {
+    "revision": "7f2a307c3d0f57453cd8",
+    "url": "/photo-gallery/static/js/main.fddea294.chunk.js"
+  },
+  {
+    "revision": "0d1c3838ca3ea1b2007e",
+    "url": "/photo-gallery/static/js/runtime~main.0958a99a.js"
+  },
+  {
+    "revision": "607b3275fd293e78108eb22b50a2c22f",
+    "url": "/photo-gallery/static/media/sad.607b3275.png"
+  }
+]);
